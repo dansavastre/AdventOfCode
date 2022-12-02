c0e24abe17c27d8feaebd946bdbf836a6fdc106f
Dan <D.Savastre@student.tudelft.nl>
2022-12-02 14:54:52 +0200

    Day 1 and 2

--- AdventOfCode_2022/main.py
+++ AdventOfCode_2022/main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def day1():
+    file1 = open('day1.in', 'r')
+    Lines = file1.readlines()
+    calories = []
+    current_sum = 0
+    for line in Lines:
+        line = line.strip()
+        # check if line is a number
+        print("Line: ", line, line.isnumeric())
+        if line.isnumeric():
+            print("Int: ", int(line))
+            current_sum += int(line)
+        else:
+            calories.append(current_sum)
+            current_sum = 0
+    # Print the top 3 values
+    print(np.sum(sorted(calories, reverse=True)[:3]))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def day2_part1():
+    file1 = open('day2.in', 'r')
+    Lines = file1.readlines()
+    score = 0
+    for line in Lines:
+        if line[0] == 'A' and line[2] == 'X':
+            score += (1 + 3)
+        elif line[0] == 'A' and line[2] == 'Y':
+            score += (2 + 6)
+        elif line[0] == 'A' and line[2] == 'Z':
+            score += (3 + 0)
+        elif line[0] == 'B' and line[2] == 'X':
+            score += (1 + 0)
+        elif line[0] == 'B' and line[2] == 'Y':
+            score += (2 + 3)
+        elif line[0] == 'B' and line[2] == 'Z':
+            score += (3 + 6)
+        elif line[0] == 'C' and line[2] == 'X':
+            score += (1 + 6)
+        elif line[0] == 'C' and line[2] == 'Y':
+            score += (2 + 0)
+        elif line[0] == 'C' and line[2] == 'Z':
+            score += (3 + 3)
+        print('Opponent: ', line[0], '| Me: ', line[2], '| Score: ', score)
+    print('Score: ', score)
 
 
-# Press the green button in the gutter to run the script.
+def day2_part2():
+    file1 = open('day2.in', 'r')
+    Lines = file1.readlines()
+    score = 0
+    for line in Lines:
+        if line[0] == 'A' and line[2] == 'X':
+            score += (3 + 0)
+        elif line[0] == 'A' and line[2] == 'Y':
+            score += (1 + 3)
+        elif line[0] == 'A' and line[2] == 'Z':
+            score += (2 + 6)
+        elif line[0] == 'B' and line[2] == 'X':
+            score += (1 + 0)
+        elif line[0] == 'B' and line[2] == 'Y':
+            score += (2 + 3)
+        elif line[0] == 'B' and line[2] == 'Z':
+            score += (3 + 6)
+        elif line[0] == 'C' and line[2] == 'X':
+            score += (2 + 0)
+        elif line[0] == 'C' and line[2] == 'Y':
+            score += (3 + 3)
+        elif line[0] == 'C' and line[2] == 'Z':
+            score += (1 + 6)
+        print('Opponent: ', line[0], '| Me: ', line[2], '| Score: ', score)
+    print('Score: ', score)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    day2_part2()
