022de34e5491246655e6e03109c5dea3041ef180
Dan <D.Savastre@student.tudelft.nl>
2022-12-05 17:11:56 +0100

    Day 5 complete implementation

--- AdventOfCode_2022/day5/main.py
+++ AdventOfCode_2022/day5/main.py
@@ -3,12 +3,12 @@ import re
 
 class move:
     """
-    docstring for move
     Class move contains the following attributes:
         - number of items to move
         - stack index to move from
         - stack index to move to
     """
+
     def __init__(self, info):
         self.number = int(info[0])
         self.from_stack = int(info[1])
@@ -22,6 +22,18 @@ class move:
 
 
 class stack:
+    """
+    Class stack contains the following attributes:
+        - stack: list of items in the stack
+        - index: index of the stack
+    Class stack contains the following methods:
+        - push: push an item to the stack
+        - pop: pop an item from the stack
+        - pop_index: pop an item from the stack at a specific index
+        - peek: peek at the top of the stack
+        - isEmpty: check if the stack is empty
+        - getStackIndex: get the index of the stack
+    """
     def __init__(self, index):
         self.stack = []
         self.index = index
@@ -31,6 +43,9 @@ class stack:
 
     def pop(self):
         return self.stack.pop()
+
+    def pop_index(self, index):
+        return self.stack.pop(index)
 
     def peek(self):
         return self.stack[-1]
@@ -59,8 +74,7 @@ if __name__ == '__main__':
     for line in lines:
         # Find the number of stacks
         if len(line) > 1 and line[1].isnumeric():
-            # Bottom of stack
-            # print('Number of stacks:', line[-2])
+            # Bottom of stacks
             n_stacks = int(line[-2])
             # Depth is the index of the line
             depth = lines.index(line) - 1
@@ -68,7 +82,6 @@ if __name__ == '__main__':
         if lines.index(line) <= depth:
             for i in range(1, len(line), 4):
                 if line[i].isupper():
-                    print('Stack {}: {}'.format((i + 3) // 4, line[i]))
                     flag = False
                     for st in stacks:
                         if st.getStackIndex() == (i + 3) // 4:
@@ -81,29 +94,37 @@ if __name__ == '__main__':
 
         # Once stacks are filled, read the moves
         if lines.index(line) > depth + 2:
-            print('Move: ', line)
             moves.append(move(re.findall(r'\d+', line)))
 
     # Flip the stacks
     for st in stacks:
         st.stack.reverse()
 
-    #Sort stacks by index
+    # Sort stacks by index
     stacks.sort(key=lambda x: x.getStackIndex())
-    print('Stacks: ', stacks)
-    print('Moves: ', moves)
 
-    # Start moving
-    for move in moves:
-        print(move)
-        for i in range(move.number):
-            stacks[move.to_stack - 1].push(stacks[move.from_stack - 1].pop())
-        print(stacks)
-
-    # Print the final result
-    # is the string of the top of each stack
-    result = ''
+    # Create new stacks for the moves
+    stacks_part_1 = []
+    stacks_part_2 = []
     for st in stacks:
-        result += st.peek()
-    print('Result: ', result)
+        stacks_part_1.append(stack(st.getStackIndex()))
+        stacks_part_2.append(stack(st.getStackIndex()))
+        for crate in st.stack:
+            stacks_part_1[-1].push(crate)
+            stacks_part_2[-1].push(crate)
 
+    # Start moving the crates
+    for move in moves:
+        for i in range(move.number, 0, -1):
+            stacks_part_1[move.to_stack - 1].push(stacks_part_1[move.from_stack - 1].pop())
+            stacks_part_2[move.to_stack - 1].push(stacks_part_2[move.from_stack - 1].pop_index(-i))
+
+    # Print the final result for part 1
+    result_part_1 = ''
+    result_part_2 = ''
+    for i in range(n_stacks):
+        result_part_1 += stacks_part_1[i].peek()
+        result_part_2 += stacks_part_2[i].peek()
+
+    print('Part 1:', result_part_1)
+    print('Part 2:', result_part_2)
